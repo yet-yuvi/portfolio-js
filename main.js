143942bde9604e9677dba8df7388a2e0ade17aaf
yet-yuvi <oscyanogen@gmail.com>
2025-09-11 05:10:21 +0600

    home page design

---
+++ main.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".text", {
+  strings: ["Frontend developer", "Backend developer", "Full stack developer."],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
